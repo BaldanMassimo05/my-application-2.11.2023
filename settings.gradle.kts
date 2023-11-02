5dce1cc1b9802a0ccb2ee0d9217686b7c449a737
Baldan Massimo <baldan@Collalto.local>
2023-11-02 13:26:43 +0100

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,17 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "esercizio 26-10"
+include(":app")
